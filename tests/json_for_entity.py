290826fac850dbda619152e6db526223c2f3cf44
Roderic Linguri <rlinguri@mac.com>
2015-05-29 20:31:23 -0600

    Add comments to tests

--- tests/json_for_entity.py
+++ tests/json_for_entity.py
@@ -8,5 +8,11 @@ import pyfi
 fm = pyfwk.FileManager.instance()
 fm.set_root(os.path.dirname(os.path.dirname(__file__)))
 
+"""
+The first time the following statement is run, the entity module will attempt to
+create the sqlite database and populate with tables. Your project directory will
+need to be writeable for this to occur.
+"""
+
 entity = pyfi.Entity(1123)
 print(json.dumps(entity.dict(), indent=4, sort_keys=True, separators=(',', ': ')))
--- tests/session_for_date.py
+++ tests/session_for_date.py
@@ -8,5 +8,11 @@ import pyfi
 fm = pyfwk.FileManager.instance()
 fm.set_root(os.path.dirname(os.path.dirname(__file__)))
 
+"""
+The first time the following statement is run, the calendar module will attempt to
+create the sqlite database and populate with tables. Your project directory will
+need to be writeable for this to occur.
+"""
+
 session = pyfi.Session(2015, 6, 1)
 print(json.dumps(session.dict(), indent=4, sort_keys=True, separators=(',', ': ')))
